4f8992cf95672ba8b79823513dbef43bb4b05a18
LuisRibeiro2903 <luisgithubribeiro@gmail.com>
2024-01-20 22:49:35 +0000

    Inserção da funcionalidade de pesquisa, para já ainda por texto

---
+++ Music4Every1/Client/Services/AuctionsService/IAuctionsService.cs
@@ -0,0 +1,12 @@
+﻿namespace Music4Every1.Client.Services.AuctionsService
+{
+    public interface IAuctionsService
+    {
+        List<Leilao> Auctions { get; set; }
+
+        Task GetAuctions();
+
+        Task FilteredSearch(Filter search);
+
+    }
+}
